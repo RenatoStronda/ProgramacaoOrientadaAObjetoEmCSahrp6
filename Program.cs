baa23f31adf950ac96ad5ffd8691815aa98a792e
RenatoStronda <104527831+RenatoStronda@users.noreply.github.com>
2022-09-12 22:36:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace heranca{
+    class Program{
+    public static void Main(string[] args) {
+        Gato g = new Gato();
+        Animal a = new Gato();
+        a.GetEmitirSom();
+        a = new Dog();
+        a.GetEmitirSom();
+    }
+}
+public abstract class Animal{
+    private Animal gato, dog;
+
+public Animal(Animal gato, Animal dog){
+    this.Animal = gato;
+    this.Animal = dog;
+}
+}
+
+public virtual void GetEmitirSom(){
+    Console.WtriteLine($"GetEmitirSom: {miau}");
+}
+
+public virtual void GetEmitirSom(){
+    Console.WtriteLine($"GetEmitirSom: {auau}");
+}
+}
+
+public class Gato : Animal
+{
+  public Gato(Animal g, Animal d) : base(g, d)
+  {
+
+  }
+  public override void GetEmitirSom(){
+    base.GetEmitirSom();
+  }
+}
+
+public class Dog : Animal
+{
+  public Dog(Animal g, Animal d) : base(g, d)
+  {
+
+  }
+  public override void GetEmitirSom(){
+    base.GetEmitirSom();
+  }
+}
